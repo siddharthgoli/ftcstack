d4e7bfa0c523c94cb5618ff888e9c14590428368
siddharthgoli <141174659+siddharthgoli@users.noreply.github.com>
2026-08-09 20:07:18 -0400

    add gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { JetBrains_Mono, Manrope } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
 import { Metadata } from "next";
 import { appName, logoSrc } from "@/app/layout.shared";
+import Script from "next/script";
 
 const manrope = Manrope({
     subsets: ["latin"],
@@ -14,6 +15,8 @@ const jetbrainsMono = JetBrains_Mono({
     subsets: ["latin"],
     variable: "--font-mono",
 });
+
+const GA_MEASUREMENT_ID = process.env.GA_MEASUREMENT_ID;
 
 export const metadata: Metadata = {
     title: appName,
@@ -30,6 +33,18 @@ export default function Layout({ children }: LayoutProps<"/">) {
             suppressHydrationWarning
         >
             <body className="flex flex-col min-h-screen">
+                <Script
+                    src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', '${GA_MEASUREMENT_ID}');
+                    `}
+                </Script>
                 <RootProvider theme={{ defaultTheme: "dark" }}>
                     {children}
                 </RootProvider>
